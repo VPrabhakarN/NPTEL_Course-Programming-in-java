b4417d033dc3c875c357c9c871f9c4a38939b305
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-08-14 20:26:40 +0530

    Add files via upload
    Java program to demonstrate the array in java.

---
+++ P2_Array.java
@@ -0,0 +1,13 @@
+// Java program to demonstrate the array
+public class P2_Array {
+    public static void main(String [] args){
+        // defining the array 
+        int arr[] = {10, 20, 30, 40, 50};
+
+        // traversing the array using for loop 
+        System.out.println("Array Elements");
+        for(int i=0; i<5; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
