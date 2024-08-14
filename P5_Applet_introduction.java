8551793a5c54af6d41ec6e556a2087ef818efc1e
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-08-14 22:03:34 +0530

    Add files via upload
    This is just the introduction of applet.

---
+++ P5_Applet_introduction.java
@@ -0,0 +1,10 @@
+// Java program to demonstrate the java applet
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class P5_Applet_introduction {
+    public void paint(Graphics g){
+        g.drawString("Hello Worlds", 150, 150);
+    }
+}
