664d067082ee65a6d20b8be99775957bebc5d04b
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-08-14 20:21:13 +0530

    Add files via upload
    This is first java program of the NPTEL course series.

---
+++ P1_Hello_World.java
@@ -0,0 +1,7 @@
+// Java program to print 'Hello World'
+public class P1_Hello_World{
+    public static void main(String args[]){
+        // printing the message 
+        System.out.println("Hello World....!");
+    }
+}
